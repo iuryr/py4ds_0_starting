1d0e5d888dac3dc5775e1af0bcb1f076d2aee007
iuryr <iuryroberto@gmail.com>
2026-02-15 10:16:08 -0300

    ex02
    - Note that I am checking the type of an object by checking member variable of the return of the builtin type() function. This method does not take into account types that are inherited

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,16 @@
+def all_thing_is_obj(object: any) -> int:
+    obj_type = type(object)
+    match obj_type.__name__:
+        case 'list':
+            print(f"List : {obj_type}")
+        case 'tuple':
+            print(f"Tuple : {obj_type}")
+        case 'set':
+            print(f"Set : {obj_type}")
+        case 'dict':
+            print(f"Dict : {obj_type}")
+        case 'str':
+            print(f"{object} is in the kitchen : {obj_type}")
+        case _:
+            print(f"Type not found")
+    return 42
