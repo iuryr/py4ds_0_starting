76b92ec581faec27d22d88832b4d4432c6eab0c3
iuryr <iuryroberto@gmail.com>
2026-02-16 14:31:07 -0300

    ex06 - using now generator comprehension instead of list comprehension

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -5,10 +5,6 @@ def ft_filter(func, iterable):
     Return an iterator yielding those items of iterable for which
     function(item) is true. If function is None, return the items that are true
     """
-    if func is None:
-        filtered = [x for x in iterable if x]
-    else:
-        filtered = [x for x in iterable if func(x)]
-
-    for x in filtered:
-        yield x
+    if func:
+        return (x for x in iterable if func(x))
+    return (x for x in iterable if x)
