92af215918b2289f774389b75413fd125de4ce2e
iuryr <iuryroberto@gmail.com>
2026-02-18 14:44:33 -0300

    ex08 refactor

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -2,9 +2,22 @@ from os import get_terminal_size
 
 def ft_tqdm(lst: range) -> None:
     COLS = get_terminal_size()[0]
-    total = len(lst)
-    for i in range(1, total + 1):
-        completion_pct = round((i/total) * 100)
-        print(f"\r{completion_pct:3}%|", end="")
+    pct_display_size = 6 # "100%|["
+    total_iterations = len(lst)
+    total_it_digits = len(str(total_iterations))
+    iter_display_size = 2 * total_it_digits + 2 #]number/number
+    #1 below is '>' char
+    bar_size = COLS - 1 - pct_display_size - iter_display_size
+
+    for i in range(1, total_iterations + 1):
+        completion_pct = round((i/total_iterations) * 100)
+        filled = round((completion_pct/100) * bar_size)
+
+        pct_display = f"\r\x1b[2K{completion_pct:3}%|["
+        bar = "=" * filled + ">" + " " * (bar_size - filled)
+        iter_display = f"]{i:3}/{total_iterations}"
+
+        print(pct_display, bar, iter_display,sep="", end="", flush=True)
+
         yield 
 
--- ex08/tester.py
+++ ex08/tester.py
@@ -3,12 +3,14 @@ from time import sleep
 
 from Loading import ft_tqdm
 
-# for elem in tqdm(range(333)):
-#     # print("something")
-#     sleep(0.5)
-# print()
-
-for elem in ft_tqdm(range(333)):
+for elem in ft_tqdm(range(333, 0, -1)):
     sleep(0.005)
+print()
+
+for elem in tqdm(range(333,0, -1)):
+    # print("something")
+    sleep(0.005)
+print()
+
 
 
