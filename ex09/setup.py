3a3acf0f68230ab8c5ff29228675a1f7fcffceb5
iuryr <iuryroberto@gmail.com>
2026-02-19 20:29:47 -0300

    finally, i guess

--- ex09/setup.py
+++ ex09/setup.py
@@ -1,4 +1,3 @@
 from setuptools import setup
 
-setup(
-        url="https://github.com/iuryr/py4ds_0_starting/tree/main/ex09")
+setup(url="https://github.com/iuryr/py4ds_0_starting/tree/main/ex09")
--- ex09/src/ft_package/code.py
+++ ex09/src/ft_package/code.py
@@ -1,3 +1,5 @@
-def count_in_list(lst : list[str], word : str) -> int:
+def count_in_list(lst: list[str], word: str) -> int:
+    """Counts ocurrences of word in a list of words
+    """
     count = sum([1 for string in lst if string == word])
     return count
