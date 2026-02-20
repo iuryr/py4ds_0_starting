3f876887d5e167ee14401fedeb85bb2d0e201bd3
iuryr <iuryroberto@gmail.com>
2026-02-20 09:33:30 -0300

    forgot builtin method existed

--- ex09/src/ft_package/code.py
+++ ex09/src/ft_package/code.py
@@ -1,5 +1,4 @@
 def count_in_list(lst: list[str], word: str) -> int:
     """Counts ocurrences of word in a list of words
     """
-    count = sum([1 for string in lst if string == word])
-    return count
+    return lst.count(word)
